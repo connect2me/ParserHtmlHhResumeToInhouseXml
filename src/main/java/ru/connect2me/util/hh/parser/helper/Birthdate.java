abdd0de14e26196ca0e75125f008c1b69aa1b27e
r.zaynullin <r.zaynullin@Home_Lenovo>
2012-12-03 13:42:50 +0400

    коммит 5

--- src/main/java/ru/connect2me/util/hh/parser/helper/Birthdate.java
+++ src/main/java/ru/connect2me/util/hh/parser/helper/Birthdate.java
@@ -2,7 +2,7 @@ package ru.connect2me.util.hh.parser.helper;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import ru.connect2me.util.hh.parser.config.ParserHtmlHhResumeToInhouseXmlException;
+import ru.connect2me.util.hh.parser.helper.enums.EnumFormatInputDate;
 
 /**
  * Получение дня рождения из резюме
@@ -10,21 +10,20 @@ import ru.connect2me.util.hh.parser.config.ParserHtmlHhResumeToInhouseXmlExcepti
  * @author Зайнуллин Радик
  */
 public class Birthdate {
-  public String get(String txt) throws ParserHtmlHhResumeToInhouseXmlException {
-    String resultRussian = getRussian(txt);
-    String resultEnglish = getEnglish(txt);
-    if (resultRussian.equals("not found")) return resultEnglish;
-    else return "not found"; 
-  }   
-  // (23 of March 1984)
-  private String getRussian(String txt){
-    Matcher matcher = Pattern.compile("\\((\\d{1,2} (января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря) 19\\d{2})\\)").matcher(txt);
-    if (matcher.find()) return matcher.group(1);
-    else return "not found";
-  }
-  private String getEnglish(String txt){
-    Matcher matcher = Pattern.compile("\\((\\d{1,2} of (January|February|March|April|May|June|July|August|September|October|November|December) 19\\d{2})\\)").matcher(txt);
-    if (matcher.find()) return matcher.group(1);
-    else return "not found";
+  public String get(String txt){
+    Matcher matcher = Pattern.compile("\\((\\d{1,2} of (January|February|March|April|May|June|July|August|September|October|November|December) 19\\d{2})\\)|"
+            + "\\((\\d{1,2} (января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря) 19\\d{2})\\)").matcher(txt);
+    String formattedDate = "not found"; 
+    if (matcher.find()) {
+      for (EnumFormatInputDate cur: EnumFormatInputDate.values()) {
+        String str = matcher.group(1);
+        if (str == null) str = matcher.group(2);
+        System.out.println("str " + str);
+        System.out.println("cur.regexp() " + cur.regexp());
+        System.out.println("cur.replacement() " + cur.replacement());
+        formattedDate = str.replaceAll(cur.regexp(), cur.replacement());
+      }
+    } 
+    return formattedDate;
   }  
 }
---
+++ src/main/java/ru/connect2me/util/hh/parser/helper/enums/EnumFormatInputDate.java
@@ -0,0 +1,40 @@
+package ru.connect2me.util.hh.parser.helper.enums;
+
+/**
+ * регулярные выражения для преобразований дат в виде 21 декабря 1953 в 21.12.1953
+ * то же самое если месяц написан по английски
+ * @author Зайнуллин Радик 
+ * @since 2012.12.03
+ */
+public enum EnumFormatInputDate {
+  January("(?i)(\\d{1,2}) (января|January) (19\\d{2})", "$1.01.$2"),
+  February("(?i)(\\d{1,2}) (февраля|February) (19\\d{2})", "$1.01.$2"),
+  March("(?i)(\\d{1,2}) (марта|March) (19\\d{2})", "$1.01.$2"),
+  April("(?i)(\\d{1,2}) (апреля|April) (19\\d{2})", "$1.01.$2"),
+  May("(?i)(\\d{1,2}) (мая|May) (19\\d{2})", "$1.01.$2"),
+  June("(?i)(\\d{1,2}) (июня|June) (19\\d{2})", "$1.01.$2"),
+  July("(?i)(\\d{1,2}) (июля|July) (19\\d{2})", "$1.01.$2"),
+  August("(?i)(\\d{1,2}) (августа|August) (19\\d{2})", "$1.01.$2"),
+  September("(?i)(\\d{1,2}) (сентября|September) (19\\d{2})", "$1.01.$2"),
+  October("(?i)(\\d{1,2}) (октября|October) (19\\d{2})", "$1.01.$2"),
+  November("(?i)(\\d{1,2}) (ноября|November) (19\\d{2})", "$1.01.$2"),
+  December("(\\d{1,2}) (декабря|December) (19\\d{2})", "$1.01.$2");
+  
+  private final String regexp;      // искомое выражение
+  private final String replacement; // замена
+
+  EnumFormatInputDate(String regexp, String replacement) {
+    this.regexp = regexp;
+    this.replacement = replacement;
+  }
+
+  public String regexp() {
+    return regexp;
+  }
+
+  public String replacement() {
+    StringBuilder sb = new StringBuilder(replacement);
+    if (sb.toString().length()<10) sb.insert(0, "0");
+    return sb.toString();
+  }  
+}
